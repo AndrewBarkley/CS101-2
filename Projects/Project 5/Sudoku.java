456827afb2a459e0c759836929fd96a7ce1b5de6
AndrewBarkley <zmosley6@gmail.com>
2016-09-12 01:03:44 -0400

    It is FINISHED

--- Projects/Project 5/Sudoku.java
+++ Projects/Project 5/Sudoku.java
@@ -3,17 +3,47 @@
 import java.util.*;
 import java.io.*;
 
+   /*
+                     Class Algorithm
+   -----------------------------------------------------------------------
+   int [][] values = new int [9][9]
+   boolean [][] editable = new boolean [9][9]
+   boolean full
+   int trueCount
+   -----------------------------------------------------------------------
+
+                     Class Data Table
+   Variable or Constant          Purpose
+   ____________________          _________________________________________
+   int [][] values               array to keep track of Sudoku values
+   boolean [][] editable         array to keep track of which values are editable
+   boolean full                  keeps track if the puzzle is full
+   int trueCount                 keeps track of th number of possible values for one cell
+
+   */
+
 public class Sudoku
 {
-   //class variables
    private int [][] values = new int [9][9];
    private boolean [][] editable = new boolean [9][9];
    private boolean full;
    private int trueCount;
    
-   //class methods
-   
-   public Sudoku()//done
+   /*
+                     Sudoku constructor Algorithm
+   -----------------------------------------------------------------------
+   for(int r=0;r<9;r++)
+         for(int c=0;c<9;c++)
+            editable[r][c] <- true
+   -----------------------------------------------------------------------
+
+                     Sudoku constructor Data Table
+   Variable or Constant          Purpose
+   ____________________          _________________________________________
+   boolean [][] editable         array to keep track of which values are editable
+
+   */
+   public Sudoku()
    {
       for(int r=0;r<9;r++)
       {
@@ -24,7 +54,27 @@ public class Sudoku
       }
    }
    
-   public void addInitial(int row, int coloumn, int value)//done
+   /*
+                     addInitial Algorithm
+   -----------------------------------------------------------------------
+   if(0<row<10 && 0<coloumn<10 && 0<value<10)
+      values[row-1][coloumn-1] <- value
+      editable[row-1][coloumn-1] <- false
+   else
+      print <- ERROR message
+   -----------------------------------------------------------------------
+
+                     addInitial Data Table
+   Variable or Constant          Purpose
+   ____________________          _________________________________________
+   int row                       determines which row to changes
+   int coloumn                   determines what coloumn to change
+   int value                     determines what to change the cell to
+   values[][]                    array to keep track of Sudoku values
+   editable[][]                  array to keep track of which values are editable
+
+   */
+   public void addInitial(int row, int coloumn, int value)
    {
       if(0<row && row<10 && 0<coloumn && coloumn<10 && 0<value && value<10)
       {
@@ -35,7 +85,29 @@ public class Sudoku
          System.out.println("ERROR 323: One or more value(s) aren't within bounds");
    }
    
-   public void addGuess(int row, int coloumn, int value)//done
+   /*
+                     addGuess Algorithm
+   -----------------------------------------------------------------------
+   if(editable[row-1][coloumn-1])
+      if(0<row<10 && 0<coloumn<10 && 0<value<10)
+         values[row-1][coloumn-1] <- value
+      else
+         print <- ERROR message
+   else
+      print <- ERROR message
+   -----------------------------------------------------------------------
+
+                     addGuess Data Table
+   Variable or Constant          Purpose
+   ____________________          _________________________________________
+   int row                       determines which row to changes
+   int coloumn                   determines what coloumn to change
+   int value                     determines what to change the cell to
+   values[][]                    array to keep track of Sudoku values
+   editable[][]                  array to keep track of which values are editable
+
+   */
+   public void addGuess(int row, int coloumn, int value)
    {
       if(editable[row-1][coloumn-1])
       {
@@ -50,7 +122,24 @@ public class Sudoku
          System.out.println("ERROR 507: Cell is not editable");
    }
    
-   public int getValueIn(int row, int coloumn)//done
+   /*
+                     getValueIn Algorithm
+   -----------------------------------------------------------------------
+   if(0<row<10 && 0<coloumn<10)
+      return <- values[row-1][coloumn-1];
+   else
+      return <- 323;
+   -----------------------------------------------------------------------
+
+                     getValueIn Data Table
+   Variable or Constant          Purpose
+   ____________________          _________________________________________
+   int row                       determines which row to change
+   int coloumn                   determines what coloumn to change
+   values[][]                    array to keep track of Sudoku values
+
+   */
+   public int getValueIn(int row, int coloumn)
    {
       if(0<row && row<10 && 0<coloumn && coloumn<10)
       {
@@ -60,7 +149,103 @@ public class Sudoku
          return 323;
    }
    
-   public String getAllowedValues(int row, int coloumn)//done
+   /*
+                     getAllowedValues Algorithm
+   -----------------------------------------------------------------------
+      row--
+      coloumn--
+      boolean [] possible <- new boolean [9]
+      trueCount <- 0
+      for(int i=0;i<9;i++)
+         possible[i] <- true
+         trueCount++
+      
+      //row
+      for(int c <- 0;c<9;c++)
+         if(values[row][c]>0 && c!=coloumn)
+            if(possible[values[row][c]-1])
+               possible[values[row][c]-1] <- false
+               trueCount--
+      
+      //coloumn
+      for(int r <- 0;r<9;r++)
+         if(values[r][coloumn]>0 && r!=row)
+            if(possible[values[r][coloumn]-1])
+               possible[values[r][coloumn]-1] <- false
+               trueCount--
+               
+      //box
+      int box <- getBox(row,coloumn)
+      int rowI
+      int coloumnI
+      switch(box)
+         case 1:
+            rowI <- 0
+            coloumnI <- 0 
+            break
+         case 2:
+            rowI <- 0
+            coloumnI <- 3 
+            break
+         case 3:
+            rowI <- 0
+            coloumnI <- 6 
+            break
+         case 4:
+            rowI <- 3
+            coloumnI <- 0 
+            break
+         case 5:
+            rowI <- 3
+            coloumnI <- 3
+            break
+         case 6:
+            rowI <- 3
+            coloumnI <- 6
+            break
+         case 7:
+            rowI <- 6
+            coloumnI <- 0
+            break
+         case 8:
+            rowI <- 6
+            coloumnI <- 3
+            break
+         default:
+            rowI <- 6
+            coloumnI <- 6
+            break
+      
+      for(int r <- rowI;r<(rowI+3);r++)
+         for(int c <- coloumnI;c<(coloumnI+3);c++)
+            if(values[r][c]!=0)
+               if(r!=row && c!=coloumn)
+                  if(possible[values[r][c]-1])
+                     possible[values[r][c]-1] <- false
+                     trueCount--
+       
+      String output <- ""
+      for(int i <- 1;i<=9;i++)
+         output += (i) + "." + possible[i-1] + ", "
+       
+      return <- output
+   -----------------------------------------------------------------------
+
+                     getAllowedValues Data Table
+   Variable or Constant          Purpose
+   ____________________          _________________________________________
+   int row                       determines the cell's row
+   int coloumn                   determines the cell's coloumn
+   int rowI                      the upper row of the cell's box
+   int coloumnI                  the left most coloumn of the cell's box
+   int trueCount                 counts the number of possible values for a cell
+   values[][]                    array to keep track of Sudoku values
+   possible[][]                  array to keep track of the cell's possible values
+   editable[][]                  array to keep track of which values are editable
+   String output                 String verion of possible[][]
+
+   */
+   public String getAllowedValues(int row, int coloumn)
    {
       row--;
       coloumn--;
@@ -170,10 +355,31 @@ public class Sudoku
       return output;
    }
    
-   public boolean checkPuzzle()//done
+   /*
+                     checkPuzzle Algorithm
+   -----------------------------------------------------------------------
+   if(isFull())
+      boolean allCorrect = true
+      for(int r <- 1;r<=9;r++)
+         for(int c <- 1;c<=9;c++)
+            getAllowedValues(r,c)
+            if(trueCount != 1)
+               allCorrect <- false
+      return <- allCorrect
+   return <- false
+   -----------------------------------------------------------------------
+
+                     checkPuzzle Data Table
+   Variable or Constant          Purpose
+   ____________________          _________________________________________
+   boolean allCorrect            determines which row to changes
+   int trueCount                 counts the number of possible values for a cell
+
+   */
+   public boolean checkPuzzle()
    {
-      //if(isFull())
-      //{
+      if(isFull())
+      {
          boolean allCorrect = true;
          for(int r=1;r<=9;r++)
          {
@@ -189,11 +395,29 @@ public class Sudoku
             }
          }
          return allCorrect;
-      //}
-      //return false;
+      }
+      return false;
    }
    
-   public boolean isFull()//done
+   /*
+                     isFull Algorithm
+   -----------------------------------------------------------------------
+      full <- true;
+      for(int r <- 0;r<9;r++)
+         for(int c <- 0;c<9;c++)
+            if(values[r][c] != 0)
+               full <- false
+      return <- full
+   -----------------------------------------------------------------------
+
+                     isFull Data Table
+   Variable or Constant          Purpose
+   ____________________          _________________________________________
+   int full                      keeps tracks if the puzzle is full
+   values[][]                    array to keep track of Sudoku values
+
+   */
+   public boolean isFull()
    {
       full = true;
       for(int r=0;r<9;r++)
@@ -207,7 +431,23 @@ public class Sudoku
       return full;
    }
    
-   public void reset()//Unsure why, but doesnt work
+   /*
+                     reset Algorithm
+   -----------------------------------------------------------------------
+      for(int r <- 0;r<9;r++)
+         for(int c <- 0;c<9;c++)
+            if(editable[r][c])
+               values[r][c] <- 0
+   -----------------------------------------------------------------------
+
+                     reset Data Table
+   Variable or Constant          Purpose
+   ____________________          _________________________________________
+   values[][]                    array to keep track of Sudoku values
+   editable[][]                  array to keep track of which values are editable
+
+   */
+   public void reset()
    {
       for(int r=0;r<9;r++)
       {
@@ -219,7 +459,33 @@ public class Sudoku
       }
    }
    
-   public String toString()//done
+   /*
+                     toString Algorithm
+   -----------------------------------------------------------------------
+      String visual <- ""
+      for(int r <- 0;r<9;r++)
+         for(int c <- 0;c<9;c++)
+            if(values[r][c] != 0)
+               visual += " " + values[r][c]
+            else
+               visual += " _"
+            if(c==2 or c==5)
+               visual += " |"
+            if(c==8)
+               visual += " \n"
+         if(r==2 or r==5)
+            visual += "-------+-------+-------\n"
+      return <- visual
+   -----------------------------------------------------------------------
+
+                     toString Data Table
+   Variable or Constant          Purpose
+   ____________________          _________________________________________
+   values[][]                    array to keep track of Sudoku values
+   String visual                 turns the values[][] into a String to print
+
+   */
+   public String toString()
    {
       String visual = "";
       for(int r=0;r<9;r++)
@@ -242,6 +508,42 @@ public class Sudoku
       return visual;
    }
 
+   /*
+                     toString Algorithm
+   -----------------------------------------------------------------------
+      int box <- 0
+      if(row>=0 && row<=2)
+         if(coloumn>=0 && coloumn<=2)
+            box <- 1
+         if(coloumn>=3 && coloumn<=5)
+            box <- 2
+         if(coloumn>=6 && coloumn<=8)
+            box <- 3
+      if(row>=3 && row<=5)
+         if(coloumn>=0 && coloumn<=2)
+            box <- 4
+         if(coloumn>=3 && coloumn<=5)
+            box <- 5
+         if(coloumn>=6 && coloumn<=8)
+            box <- 6
+      if(row>=6 && row<=8)
+         if(coloumn>=0 && coloumn<=2)
+            box <- 6
+         if(coloumn>=3 && coloumn<=5)
+            box <- 7
+         if(coloumn>=6 && coloumn<=8)
+            box <- 8
+      return <- box
+   -----------------------------------------------------------------------
+
+                     toString Data Table
+   Variable or Constant          Purpose
+   ____________________          _________________________________________
+   int row                       determines which row to changes
+   int coloumn                   determines what coloumn to change
+   int box                       value to keep track of which box the cell is in
+
+   */
    public int getBox(int row, int coloumn)
    {
       int box = 0;
--- Projects/Project 5/SudokuDriver.java
+++ Projects/Project 5/SudokuDriver.java
@@ -4,23 +4,75 @@ import java.util.*;
 
 public class SudokuDriver
 {
+   /*
+                     main Algorithm
+   -----------------------------------------------------------------------
+   Sudoku puzzle = new Sudoku()
+   print <- puzzle
+   initialize(puzzle)
+   play(puzzle)
+   -----------------------------------------------------------------------
+
+                     main Data Table
+   Variable or Constant          Purpose
+   ____________________          _________________________________________
+   Sudoku puzzle                 Creates the Sudoku object dubbed puzzle
+
+   */
    public static void main(String [] args)
    {
-      Sudoku s = new Sudoku();
-      System.out.println(s);
+      Sudoku puzzle = new Sudoku();
+      System.out.println(puzzle);
       
-      initialize(s);
-      play(s);
+      initialize(puzzle);
+      play(puzzle);
    }
-      
-   public static void initialize(Sudoku s)
+   /*
+                     initialize Algorithm
+   -----------------------------------------------------------------------
+      boolean setup <- true
+      while(setup)
+         print <- instructions                         
+         Scanner in <- keyboard
+         int row <- 0
+         int coloumn <- 0
+         int value <- 0
+         if(in.hasNextInt())
+            row <- in.nextInt()
+            coloumn <- in.nextInt()
+            value <- in.nextInt()
+            s.addInitial(row,coloumn,value)
+            print <- s 
+         String input <- in.nextLine()
+         if(input.toLowerCase().contains("play"))
+            setup <- false
+         else if (input.equals(""))
+               //empty, only here to prevent false error messages
+         else
+            print <- "ERROR 503: Unknown command"
+            
+         print <- "" //to skip a line
+   -----------------------------------------------------------------------
+
+                     initialize Data Table
+   Variable or Constant          Purpose
+   ____________________          _________________________________________
+   boolean setup                 used in the while statement to keep initialize running
+   Scanner in                    gets keyboard input
+   int row                       determines which row to changes
+   int coloumn                   determines what coloumn to change
+   int value                     determines what to change the cell to
+   String input                  
+
+   */   
+   public static void initialize(Sudoku puzzle)
    {
       boolean setup = true;
       while(setup)
       {
          System.out.println("Type 'play' to start the game,\n" +
                             "type a set of three numbers to initialize a cell\n" +
-                            "Ex. Row Coloumn Value) ");
+                            "Ex. Row Coloumn Value ");
                                          
          Scanner in = new Scanner(System.in);
          int row = 0;
@@ -34,8 +86,8 @@ public class SudokuDriver
             coloumn = in.nextInt();
             value = in.nextInt();
             
-            s.addInitial(row,coloumn,value);
-            System.out.println(s);
+            puzzle.addInitial(row,coloumn,value);
+            System.out.println(puzzle);
          }
          
          String input = in.nextLine();
@@ -52,8 +104,84 @@ public class SudokuDriver
          System.out.println();
       }
    }   
+   /*
+                     initialize Algorithm
+   -----------------------------------------------------------------------
+      boolean run <- true
+      while(run)
+         print <- instructions             
+         Scanner in <- keyboard
+         int input <- 0
+         if(in.hasNextInt())
+            input <- in.nextInt()
+         int row <- 0
+         int coloumn <- 0
+         int value <- 0
+         
+         switch(input)
+            case 1:
+               print <- instructions
+               row <- in.nextInt()
+               coloumn <- in.nextInt()
+               value <- in.nextInt()
+               puzzle.addGuess(row,coloumn,value)
+               break
+            case 2:
+               print <- instructions
+               row <- in.nextInt()
+               coloumn <- in.nextInt()
+            
+               if(puzzle.getValueIn(row,coloumn) <- 323)
+                  print <- ERROR message
+               else
+                  print <- puzzle.getValueIn(row,coloumn))
+               break
+            case 3:
+               print <- instructions
+               row <- in.nextInt()
+               coloumn <- in.nextInt()
+               print <- puzzle.getAllowedValues(row,coloumn))
+               break
+            case 4:
+               print <- puzzle.isFull())
+               break
+            case 5:
+               if(puzzle.checkPuzzle())
+                  print <- "Hooray :) You've won!!! Terminating game..."
+                  run <- false
+               else
+                  print <- "Sorry but you're not quite there"
+               break
+            case 6:
+               puzzle.reset()
+               print <- "Puzzle is reset"
+               break
+            case 8008:
+               print <- "Quitters never win"
+               run <- false
+               break
+            default:
+               print <- ERROR message
+               break
+         }
+         print <- empty line
+         print <- puzzle
       
-   public static void play(Sudoku s)
+   -----------------------------------------------------------------------
+
+                     initialize Data Table
+   Variable or Constant          Purpose
+   ____________________          _________________________________________
+   boolean run                   used in the while statement to keep initialize running
+   Scanner in                    gets keyboard input
+   int input                     String for switch containg operators choice
+   int row                       determines which row to changes
+   int coloumn                   determines what coloumn to change
+   int value                     determines what to change the cell to
+   String input                  
+
+   */       
+   public static void play(Sudoku puzzle)
    {  
       boolean run = true;
       while(run)
@@ -84,7 +212,7 @@ public class SudokuDriver
                coloumn = in.nextInt();
                value = in.nextInt();
             
-               s.addGuess(row,coloumn,value);
+               puzzle.addGuess(row,coloumn,value);
                break;
             case 2:
                System.out.println("type a set of two numbers to choose a cell\n" +
@@ -95,7 +223,7 @@ public class SudokuDriver
                if(s.getValueIn(row,coloumn) == 323)
                   System.out.println("ERROR 323: One or more value(s) aren't within bounds");
                else
-                  System.out.println(s.getValueIn(row,coloumn));
+                  System.out.println(puzzle.getValueIn(row,coloumn));
                break;
             case 3:
                System.out.println("type a set of two numbers to choose a cell you cheater\n" +
@@ -103,13 +231,13 @@ public class SudokuDriver
                row = in.nextInt();
                coloumn = in.nextInt();
                
-               System.out.println(s.getAllowedValues(row,coloumn));
+               System.out.println(puzzle.getAllowedValues(row,coloumn));
                break;
             case 4:
-               System.out.println(s.isFull());
+               System.out.println(puzzle.isFull());
                break;
             case 5:
-               if(s.checkPuzzle())
+               if(puzzle.checkPuzzle())
                {
                   System.out.println("Hooray :) You've won!!! Terminating game...");
                   run = false;
@@ -120,7 +248,7 @@ public class SudokuDriver
                }
                break;
             case 6:
-               s.reset();
+               puzzle.reset();
                System.out.println("Puzzle is reset");
                break;
             case 8008:
@@ -128,11 +256,11 @@ public class SudokuDriver
                run = false;
                break;
             default:
-               System.out.println("That's not on the list you boob");
+               System.out.println("ERROR 503: Unknown command");
                break;
          }
          System.out.println();
-         System.out.println(s);
+         System.out.println(puzzle);
       }
    }
 }
